d3884401ea82ca8973f810f429a8ee3ab659d4a5
radicaltray <radicaltray@gmail.com>
2026-08-26 18:54:25 +0700

    show pin status on web

--- src/pin_status.cpp
+++ src/pin_status.cpp
@@ -1,54 +1,14 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <WebSocketsServer.h>
+#include <hardware/gpio.h>
 
-struct PinState {
-    int d[6]; // Digital, GPIO 20 - 25
-    int a[4]; // Analog, GPIO 26 - 29
-
-    void poll() {
-        // TODO: add pin mode
-        for (int i = 0; i < 6; i++) {
-            this->d[i] = digitalRead(20 + i);
-        }
-        for (int i = 0; i < 4; i++) {
-            this->a[i] = analogRead(26 + i);
-        }
-    }
-
-    bool equal(PinState rhs) {
-        for (int i = 0; i < 4; i++) {
-            if (this->a[i] != rhs.a[i])
-                return false;
-        }
-        for (int i = 0; i < 6; i++) {
-            if (this->d[i] != rhs.d[i])
-                return false;
-        }
-        return true;
-    }
-
-    void dump() {
-        Serial.print("Pin:\n");
-        for (int i = 0; i < 4; i++) {
-            Serial.printf("\tA%d = %d\n", i, a[i]);
-        }
-        for (int i = 0; i < 6; i++) {
-            Serial.printf("\tD%d = %d\n", i, d[i]);
-        }
-    }
-};
-
-static PinState prevPinState{};
-static PinState currPinState{};
-
-static WebSocketsServer pinWebSocket(82);
-
+// NOTE: Does not work.
 // Get pin mode, stolen from https://github.com/arduino/ArduinoCore-API/issues/179
 //
 // On success returns: INPUT, INPUT_PULLUP, OUTPUT
 // On invalid pins returns: -1
-// static int pinMode(uint8_t pin) {
+// static int getPinMode(uint8_t pin) {
 //     if (pin >= NUM_DIGITAL_PINS) return (-1);
 //
 //     uint8_t bit = digitalPinToBitMask(pin);
@@ -60,25 +20,84 @@ static WebSocketsServer pinWebSocket(82);
 //     return ((*out & bit) ? INPUT_PULLUP : INPUT);
 // }
 
+struct PinInfo {
+    int mode;
+    int value;
+
+    bool equal(PinInfo rhs) {
+        return this->mode == rhs.mode && this->value == rhs.value;
+    }
+
+    JsonDocument json() {
+        JsonDocument doc;
+        doc["mode"] = mode;
+        doc["value"] = value;
+        return doc;
+    }
+};
+
+struct PinState {
+    PinInfo d[6]; // Digital, GPIO 20 - 25
+    PinInfo a[4]; // Analog, GPIO 26 - 29
+
+    void poll() {
+        for (int i = 0; i < 6; i++) {
+            int pin = 20 + i;
+            this->d[i].mode = gpio_is_dir_out(pin) ? 1 : 0;
+            this->d[i].value = digitalRead(pin);
+        }
+        for (int i = 0; i < 4; i++) {
+            int pin = 26 + i;
+            this->a[i].mode = gpio_is_dir_out(pin) ? 1 : 0;
+            this->a[i].value = analogRead(pin);
+        }
+    }
+
+    bool equal(PinState rhs) {
+        for (int i = 0; i < 6; i++) {
+            if (!this->d[i].equal(rhs.d[i]))
+                return false;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (!this->a[i].equal(rhs.a[i]))
+                return false;
+        }
+        return true;
+    }
+
+    void dump() {
+        Serial.print("Pin:\n");
+        for (int i = 0; i < 6; i++) {
+            Serial.printf("\tD%d = %d\n", i, d[i]);
+        }
+        for (int i = 0; i < 4; i++) {
+            Serial.printf("\tA%d = %d\n", i, a[i]);
+        }
+    }
+};
+
+static PinState prevPinState{};
+static PinState currPinState{};
+
+static WebSocketsServer pinWebSocket(82);
+
 static void sendUpdate(PinState pinState) {
     JsonDocument doc;
 
-    doc["A0"] = pinState.a[0];
-    doc["A1"] = pinState.a[1];
-    doc["A2"] = pinState.a[2];
-    doc["A3"] = pinState.a[3];
-    doc["D0"] = pinState.d[0];
-    doc["D1"] = pinState.d[1];
-    doc["D2"] = pinState.d[2];
-    doc["D3"] = pinState.d[3];
-    doc["D4"] = pinState.d[4];
-    doc["D5"] = pinState.d[5];
+    for (int i = 0; i < 6; i++) {
+        String key = "D";
+        doc[key + i] = pinState.d[i].json();
+    }
+
+    for (int i = 0; i < 4; i++) {
+        String key = "A";
+        doc[key + i] = pinState.a[i].json();
+    }
 
     String output;
     serializeJson(doc, output);
 
-    Serial.print("Sending Update");
-    Serial.println(output);
+    Serial.printf("Sending update: %s\n", output.c_str());
 
     pinWebSocket.broadcastTXT(output);
 }
